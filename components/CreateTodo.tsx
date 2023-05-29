190a7aae2069c8710798d6d97a9d0de13db39a9c
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-05-29 14:03:10 +0200

    change background color

--- components/CreateTodo.tsx
+++ components/CreateTodo.tsx
@@ -7,10 +7,10 @@ import React from 'react';
 const CreateTodo = () => {
   return (
     <div className='flex flex-col justify-center items-center gap-3 w-[90%]'>
-      <label htmlFor='createTodo' className='self-start'>
+      <label htmlFor='createTodo' className='self-start text-2xl mb-5'>
         Create a todo
       </label>
-      <div className='border flex justify-center items-center w-full gap-2 py-4 px-3 cursor-pointer'>
+      <div className='border bg-slate-700 flex justify-center items-center w-full gap-2 py-4 px-3 cursor-pointer'>
         <BsFillPlusCircleFill
           size={40}
           color='rgb(71 85 105)'
@@ -20,7 +20,7 @@ const CreateTodo = () => {
           type='text'
           id='createTodo'
           name='createTodo'
-          className='w-full h-10 rounded px-3 bg-black focus:border-b-white focus:border-b-2 active:border-b-white active:border-b-2 outline-none'
+          className='w-full h-10 rounded px-3 bg-slate-700 focus:border-b-white focus:border-b-2 active:border-b-white active:border-b-2 outline-none'
           placeholder='What do you need to accomplish today?'
         />
       </div>
