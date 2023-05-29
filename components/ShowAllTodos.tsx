b636bd9eb643f9f60a6bba0fceef9d8e4bed2629
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-05-29 14:56:05 +0200

    let users see their todos and add functionallity to check the todo is finished

--- components/ShowAllTodos.tsx
+++ components/ShowAllTodos.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+'use client';
+import { Todo } from '@/utils';
+import React, { useState } from 'react';
 
-const ShowAllTodos = () => {
+const ShowAllTodos = ({ todos }: any) => {
+  const [todoList, setTodoList] = useState(todos);
+
+  const handleToggleCompleted = (index: number) => {
+    const updatedTodos = [...todoList];
+    updatedTodos[index].isCompleted = !updatedTodos[index].isCompleted;
+    setTodoList(updatedTodos);
+  };
+
   return (
-    <div>
-      <h1>This is all my todos</h1>
+    <div className='border w-[90%] flex flex-col p-5 mx-auto'>
+      {todoList &&
+        todoList.map((todo: Todo, index: number) => (
+          <div key={index} className='flex gap-4'>
+            <input
+              type='checkbox'
+              name='completed'
+              id={`completed-${index}`}
+              onChange={() => handleToggleCompleted(index)}
+              checked={todo.isCompleted}
+            />
+            <p className={todo.isCompleted ? 'line-through' : ''}>
+              {todo.title}
+            </p>
+            <p>{todo.createdAt}</p>
+            <p>{todo.isCompleted.toString()}</p>
+          </div>
+        ))}
     </div>
   );
 };
