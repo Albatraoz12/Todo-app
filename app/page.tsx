1449eed1da2459776fa3638a211cc3ed26bd1127
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-05-29 14:06:31 +0200

    component created to show all todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import CreateTodo from '@/components/CreateTodo';
+import ShowAllTodos from '@/components/ShowAllTodos';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='container flex min-h-screen flex-col items-center justify-between mx-auto mt-10'>
+    <main className='container flex flex-col items-center justify-between mx-auto mt-10 gap-10'>
       <section className='w-full'>
         <CreateTodo />
+      </section>
+      <section className='mt-5'>
+        <ShowAllTodos />
       </section>
     </main>
   );
---
+++ components/ShowAllTodos.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShowAllTodos = () => {
+  return (
+    <div>
+      <h1>This is all my todos</h1>
+    </div>
+  );
+};
+
+export default ShowAllTodos;
